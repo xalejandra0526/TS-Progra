278183b293698f11c14420c8662e3dc4b41753a8
xalejandra0526 <143530873+xalejandra0526@users.noreply.github.com>
2023-09-07 07:50:20 -0600

    Actividad TS semana 5
    07/09/23

---
+++ TS5-XAHG_1259223.py
@@ -0,0 +1,15 @@
+"""TS Introducción a la programación
+7 de septiembre del 2023
+Ximena Hernández
+Practicar el uso de las instrucciones print e input en Python"""
+sNombre = input("Por favor, ingrese su nombre:")
+sEdad = input("Por favor, ingrese su edad:")
+sCarrera = input("Por favor, ingrese su carrera:")
+sCarné = input("Por favor, ingrese su carné:")
+print("Mi segundo programa")
+print("Nombre: "+sNombre)
+print("Edad: "+sEdad)
+print("Carrera: "+sCarrera)
+print("Carné: "+sCarné)
+print("Soy "+sNombre+", tengo "+sEdad+" años y estudio la carrera de "+sCarrera+".")
+print("Mi número de carné es; "+sCarné)
